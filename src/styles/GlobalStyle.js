091a4ac59751a2fbb96d82a5fadb4e9d8b640ed8
jihyelee <jihyelee.329@gmail.com>
2022-05-30 15:22:40 +0900

    Style : global css 설정

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+import reset from 'styled-reset'
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    *{
+        line-height:1.2 !important;
+        box-sizing:border-box;
+        font-family:'Spoqa Han Sans Neo', -apple-system, system-ui,
+        BlinkMacSystemFont, 'Helvetica Neue', 'Helvetica', sans-serif !important
+    }
+    a, span{color:inherit; font-size:inherit !important; font-family:inherit; }
+    img{ max-width:100%; width:100%; display:inline-block;}
+    li,ul,ol,a{text-decoration:none; list-style:none;}
+    .displaynone{display:none}
+    .wrapper{width:1250px; margin:0 auto; padding: 50px 0;}
+   
+`
+
+export default GlobalStyle
