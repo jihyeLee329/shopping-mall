975d7badf75b9f1ccebf4ae0f9542edf88b156c3
jihyelee <jihyelee.329@gmail.com>
2022-05-30 17:50:16 +0900

    Feat : atom 기본 값 설정

--- src/atoms.jsx
+++ src/atoms.jsx
@@ -1,20 +1,17 @@
 import { atom, selector } from "recoil";
 
+export const loadingAtom = atom({
+    key:'loading',
+    default : true,
+})
+
 export const brandAtom = atom({
     key:'brand',
     default :''
 })
 
-// export const brandSelector = selector({
-//     key:'brands',
-//     get:({get}) =>{
-//         const brand_name = get(brandAtom);
-//         return brand_name;
-//     },
-//     // atom 수정하는걸 도와줌, 원하는 state 가 어떤것이든지 그걸로 수정해줌
-//     set:({set}, newValue) =>{
-//         const brand_name = newValue;
-//         set(brandAtom, brand_name) //수정하고 싶은 recoil atom , 새로운 값 
-//         // set(minuteState, 10)
-//     }
-// })
+export const brandPrdAtom = atom({
+    key:'branPrd',
+    default:[],
+})
+
