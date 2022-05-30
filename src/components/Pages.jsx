eafb022a76b6903407a90413792aa9f3902edef2
jihyelee <jihyelee.329@gmail.com>
2022-05-30 22:58:15 +0900

    Feat : category route 추가

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import Products from "../pages/Products";
-import Brands from "./Brands";
+import Brands from "../pages/Brands";
+import Category from '../pages/Category'
 
 export default function Pages (){
     return (
         <Routes>
             <Route path="/" element={<Brands/ >} />
             <Route path="/products" element={<Products/ >} />
+            <Route path="/categories" element={<Category />} /> 
         </Routes>
     )
 }
