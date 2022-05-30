a6a6d6763b0eeddeb012f24f900906a4f6cc4674
jihyelee <jihyelee.329@gmail.com>
2022-05-30 22:59:01 +0900

    Feat : color filter 기능 추가

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,15 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import { fetchProducts, fetchBasic, fetchBrandPrd } from "../api/Api";
-import { brandAtom, brandPrdAtom, loadingAtom } from "../atoms";
+import { fetchProducts, fetchBasic, fetchBrandPrd, fetchBrandPrdColor } from "../api/Api";
+import { brandAtom, brandPrdAtom, colorsAtom, loadingAtom } from "../atoms";
 import styled from "styled-components";
 import Product from "../components/Product";
 import BackButton from '../components/BackButton'
 import { useEffect, useState } from "react";
+import Colors from "../components/Colors";
 
 const PrdList = styled.div`
   display: flex;
   justify-content: start;
-  max-width: 1250px;
   margin: 0 auto;
   flex-wrap: wrap;
 `;
@@ -23,29 +23,62 @@ const H1 = styled.h1`
 
 export default function Products() {
   const brandName = useRecoilValue(brandAtom);
-  // const { isLoading :loading, data:brandPrdList } = useQuery("brandPrd", fetchBrandPrd(brandName));
   const [loading, setLoading] = useRecoilState(loadingAtom);
   const [brandPrd, setBrandPrd] = useRecoilState(brandPrdAtom);
+  const [colorPrd, setColorPrd] = useState([])
+  const color = useRecoilValue(colorsAtom);
 
   useEffect(()=>{
-    if(brandName == ''){
-      fetchProducts().then(data => setBrandPrd(data))
-    }else{
+    if(brandName !== ''){
       fetchBrandPrd(brandName).then(data=>setBrandPrd(data))
     }
     setLoading(false);
     return ()=> setBrandPrd([]);
   },[]);
 
+
+  useEffect(()=>{
+    if(brandName !== '' && color !== ''){
+      fetchBrandPrdColor(brandName,color).then(data=>setColorPrd(data))
+    }
+    // const colorFilter = brandPrd.filter((colorfilter) => {
+    //   return colorfilter.color 
+    // })
+    // console.log(colorFilter)
+    return ()=> setColorPrd([]);
+  },[color]);
+
+
+  // useEffect(()=>{
+ 
+  //   // const colorFilter = brandPrd.filter((colorfilter) => {
+  //   //   return colorfilter.color 
+  //   // })
+  //   // console.log(colorFilter)
+  //   return ()=> setColorPrd([]);
+  // },[brandPrd]);
+
+
   return (
     <>
+    <div className="wrapper">
       <BackButton />
       <H1>{brandName}</H1>
       <PrdList>
-        {brandPrd.products !== undefined && brandPrd.products.map((product, idx) => (
-          <Product key={idx} {...product} />
-        ))}
+        {color !== '' && colorPrd.products !== undefined ?
+         (colorPrd.products.map((color,idx)=>(
+            <Product key={idx} {...color} />
+          ))
+          ):(
+            brandPrd.products !== undefined && 
+            brandPrd.products.map((product, idx) => (
+            <Product key={idx} {...product} />
+            ))
+          )
+        }
       </PrdList>
+      </div>
+      <Colors />
     </>
   );
 }
