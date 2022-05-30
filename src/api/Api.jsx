bb6d15e956be35c4c38106ceee1d4fa55fcb07c0
jihyelee <jihyelee.329@gmail.com>
2022-05-30 22:56:27 +0900

    Feat : color filter api 호출 함수 생성

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -10,8 +10,8 @@ export const fetchProducts = async ()=>{
   }
 }
 
-
-export const fetchBrandPrd = async (brandName)=>{
+//브랜드 상품 카테고리
+export const fetchBrandPrd = async(brandName)=>{
   try {
     const {data} = await axios.get(`${fetchBasic}/products?brand=${brandName}`)
     return data
@@ -20,13 +20,33 @@ export const fetchBrandPrd = async (brandName)=>{
   }
 }
 
-
-export const fetchCategories = ()=>{
-  return axios.get(`${fetchBasic}/categories`);
+//브랜드에서 색상 선택
+export const fetchBrandPrdColor = async(brandName, color)=>{
+  try {
+    const {data} = await axios.get(`${fetchBasic}/products?brand=${brandName}&color=${color}`)
+    return data
+  } catch(err) {
+    console.log("Error >>", err);
+  }
 }
+
+//카테고리 리스트
+export const fetchCategories =async ()=>{
+  try {
+    const {data} = await axios.get(`${fetchBasic}/categories`)
+    return data
+  } catch(err) {
+    console.log("Error >>", err);
+  }
+  // return axios.get(`${fetchBasic}/categories`);
+}
+
+//브랜드리스트
 export const fetchBrands = ()=>{
   return axios.get(`${fetchBasic}/brands`);
 }
+
+// 컬러리스트
 export const fetchColors = ()=>{
   return axios.get(`${fetchBasic}/colors`);
 }
