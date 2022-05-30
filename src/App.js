7cd2e04c2904b6ab1ee205ca5a5a6e5e6adf8c48
jihyelee <jihyelee.329@gmail.com>
2022-05-30 22:55:45 +0900

    Style : div.wrapper 제거

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import GlobalStyle from "./styles/GlobalStyle";
 import Pages from "./components/Pages";
+import Nav from './components/Nav'; 
+import { Helmet } from "react-helmet-async";
+
 function App() {
   return (
     <>
+    <Helmet>
+      <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css' rel='stylesheet' type='text/css'></link>
+    </Helmet>
     <GlobalStyle />
-    <div className="wrapper">
+      {/* <Nav /> */}
       <Pages></Pages>
-    </div>
+  
     </>
   );
 }
